3a9713d4b7cb06f456048977dddd9497804f3d39
mbortt <bortnmax@mail.ru>
2023-05-16 18:46:25 +0300

    добавили базоовый класс хбюман для реализации наследования

--- LessonOOPExampleProject/student.cpp
+++ LessonOOPExampleProject/student.cpp
@@ -10,9 +10,9 @@ int Student::getCount() {
 Student::Student() {
 	count++;
 	//cout << "default constructor" << endl;
-	name = "no name";
-	surname = "no surname";
-	age = 15;
+	setName ("no name");
+	setSurname ("no surname");
+	setAge (15);
 	avg_mark = 4;
 }
 
@@ -20,61 +20,15 @@ Student::Student() {
 Student::Student(string nm, string surnm, int a, float mark) {
 	count++;
 	//cout << "constructor with arguments" << endl;
-	name = nm;
-	surname = surnm;
-	age = a;
+	setName(nm);
+	setSurname  (surnm);
+	setAge  (a);
 	avg_mark = mark;
 }
 
-// constructor with arguments
-Student::Student(string nm, string surnm) {
-	count++;
-	//cout << "constructor with arguments" << endl;
-	name = nm;
-	surname = surnm;
-	age = 15;
-	avg_mark = 4;
-}
-
-// copy-constructor
-Student::Student(const Student& student) {
-	count++;
-	//cout << "copy-constructor" << endl;
-	name = student.name;
-	surname = student.surname;
-	age = student.age;
-	avg_mark = student.avg_mark;
-}
-
 Student::~Student() {
-	count--;
+	
 	//cout << "destructor" << endl;
-}
-
-string Student::getName() {
-	return name;
-}
-
-void Student::setName(string n) {
-	name = n;
-}
-
-string Student::getSurname() {
-	return surname;
-}
-
-void Student::setSurname(string n) {
-	surname = n;
-}
-
-int Student::getAge() {
-	return age;
-}
-
-void Student::setAge(int a) {
-	if (a > 0 && a < 110) {
-		age = a;
-	}
 }
 
 float Student::getAvgMark() {
@@ -85,21 +39,4 @@ void Student::setAvgMark(float mark) {
 	if (mark >= 0 && mark <= 10) {
 		avg_mark = mark;
 	}
-}
-
-void Student::clear() {
-	name = "no name";
-	surname = "no surname";
-	age = 0;
-	avg_mark = 0;
-}
-
-string Student::convert() {
-	string msg = "";
-	msg += name;
-	msg += " " + surname;
-	msg += " ( age = " + to_string(age);
-	msg += ", average mark = " + to_string(avg_mark);
-	msg += ")";
-	return msg;
 }
--- LessonOOPExampleProject/student.h
+++ LessonOOPExampleProject/student.h
@@ -1,34 +1,22 @@
 #pragma once
-#include "main.h"
+#include "human.h"
 
-class Student {
+class Student : public Human {
 private:
 	static int count;
 
-	string name;
-	string surname;
-	int age;
 	float avg_mark;
 
 public:
 	Student();
 	Student(string nm, string surnm, int a, float mark);
-	Student(string nm, string surnm);
-	Student(const Student& student);
 	~Student();
 
 
 	static int getCount();
 
-	string getName();
-	void setName(string n);
-	string getSurname();
-	void setSurname(string n);
-	int getAge();
-	void setAge(int a);
 	float getAvgMark();
 	void setAvgMark(float mark);
 
-	void clear();
-	string convert();
+	
 };
